a03ea8998f2b45aaf081de989b32e235b3e5c1d4
SirMorfield <jkoers@student.codam.nl>
2022-09-13 14:52:17 +0200

    fix: set window size

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -54,7 +54,7 @@ int main(int, char**) {
 #endif
 
 	// Create window with graphics context
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(constants.window_width, constants.window_height, "video doorbell by Joppe Koers", NULL, NULL);
 	if (window == NULL)
 		return 1;
 	glfwMakeContextCurrent(window);

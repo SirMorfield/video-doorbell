c518536cab7b95c1a48b3705d853efd3970b4867
SirMorfield <jkoers@student.codam.nl>
2022-09-15 12:01:15 +0200

    chore:

--- app/src/commands.cpp
+++ app/src/commands.cpp
@@ -3,6 +3,7 @@
 namespace commands {
 
 std::string ring(const std::string& client_phonenumber) {
+	std::cout << "ringing " << client_phonenumber << std::endl;
 	return "docker exec -it asterisk asterisk -rx 'channel originate sip/" + client_phonenumber + " extension 7004@internal'";
 }
 

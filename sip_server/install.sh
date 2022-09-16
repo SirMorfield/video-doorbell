a3da4d7efd3d0d2bf75b507c553b3f685983d5b7
SirMorfield <jkoers@student.codam.nl>
2022-09-16 15:34:04 +0200

    chore: organize files

--- sip_server/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $(pwd) != "**/sip_server" ]; then
-	cd sip_server
-	echo "Changed directory to sip_server"
-fi
-
-find . -not -name install.sh -type f -exec sudo cp -f {} /{} \;
-find . -not -name install.sh -type f -exec sudo chown asterisk:asterisk /{} \;

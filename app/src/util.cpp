569a8b4b974bbecd9b3a68d0e08dbc78e3f07f5a
Joppe Koers <joppe.koers@gmail.com>
2022-09-20 09:51:15 +0000

    feat: log

--- app/src/util.cpp
+++ app/src/util.cpp
@@ -14,6 +14,7 @@
 #endif
 
 std::optional<std::string> exec(const std::string& cmd) {
+	std::cout << "Exectuting: " << cmd << std::endl;
 	std::array<char, 128>					 buffer;
 	std::string								 result;
 	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);

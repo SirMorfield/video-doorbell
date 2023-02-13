03aff049fe1e8f13047f5fc5f029b15a27bcc835
Joppe Koers <joppe@joppekoers.nl>
2023-02-13 14:52:52 +0100

    Add comments and sync better

--- sync.sh
+++ sync.sh
@@ -1,2 +1,4 @@
 #/bin/bash
-rm -rf ~/git/video-doorbell/app/{app,obj} && scp -r ~/git/video-doorbell root@192.168.2.18:/root/video-doorbell
+# This script is for manually syncing a dev laptop's state to the touchscreen computer
+# we ignore obj and app because the touchscreen needs to build the files itself
+rsync -z --archive --no-links --human-readable --perms -P --exclude={'obj','app'}  ~/git/video-doorbell/ root@192.168.2.18:/root/video-doorbell/

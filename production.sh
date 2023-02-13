ec574bc20abeb55d9b93dc1d70605abc921746cb
Joppe Koers <joppe@joppekoers.nl>
2023-02-13 14:53:18 +0100

    Ignore useless cd

--- production.sh
+++ production.sh
@@ -32,9 +32,10 @@ export DISPLAY=':0'
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-cd config-generator
+# cd config-generator
 # ts-node generator.ts || exit 1
-cd ..
+# cd ..
+
 
 # install asterisk config files
 cp asterisk/sip.conf /etc/asterisk/sip.conf
